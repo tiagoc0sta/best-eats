0880b1114de0fc1125094135f748d2ad7aa5d7d1
Tiago Costa <tiagodpv@gmail.com>
2022-12-24 23:07:54 -0500

    add filter

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -57,12 +57,21 @@ const Food = () => {
       {/*Display Foods*/}
       <div className='grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4'>
         {foods.map((item, index) => (
-          <div className='border shadow-lg hover:scale-105' key={index}>
-            <img src={item.image} alt={item.name} />
-            <div>
-              <p>{item.name}</p>
+          <div
+            className='border shadow-lg rounded-lg hover:scale-105 duration-300'
+            key={index}
+          >
+            <img
+              src={item.image}
+              alt={item.name}
+              className='w-full h-[200px] object-cover rounded-t-lg '
+            />
+            <div className='flex justify-between px-2 py-4'>
+              <p className='font-bold'>{item.name}</p>
               <p>
-                <span>{item.price}</span>
+                <span className='bg-orange-500 text-white p-1 rounded-full'>
+                  {item.price}
+                </span>
               </p>
             </div>
           </div>
